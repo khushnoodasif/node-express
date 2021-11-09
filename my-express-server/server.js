95eaa15d02a60061b0b1eaa87a233c7d38b23719
Khushnood Asif <khushnood99@gmail.com>
2021-11-09 23:33:48 +0000

    .

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+
+app.get("/", function(request, response) {
+   response.send("Hello World"); 
+})
+
+app.get("/about", function(request, response) {
+   response.send("Test 1")
+})
+
+app.get("/contact", function(request, response) {
+   response.send("Test 2")
+})
+
 app.listen(3000, function () {
-   console.log('Server started');
+   console.log('Server started on port 3000');
 });
