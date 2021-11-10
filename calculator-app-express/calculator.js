4e1abf9e74d0eb4bb8e421cc3d1c172fe6c32561
Khushnood Asif <khushnood99@gmail.com>
2021-11-10 11:02:19 +0000

    added bmi calculator

--- calculator-app-express/calculator.js
+++ calculator-app-express/calculator.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/',function(req, res){
-   res.sendFile(__dirname + "/index.html");
+   res.sendFile(__dirname + "/calculator.html");
 });
 
 app.post("/", function(req, res){
@@ -16,6 +16,19 @@ app.post("/", function(req, res){
   var result = num1 + num2;
 
   res.send("The result is " + result);
+});
+
+app.get("/bmicalculator", function(req, res){
+   res.sendFile(__dirname + "/bmi-calculator.html");
+});
+
+app.post("/bmicalculator", function(req, res){
+   var weight = Number(req.body.weight);
+   var height = Number(req.body.height);
+   
+   var bmi = weight / (height * height);
+
+   res.send("Your BMI is " + bmi);
 });
 
 app.listen(3000,function() {
